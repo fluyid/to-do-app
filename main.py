9ca6516870247836c6cd4d75ca902adc21fc1b47
fluyid <kailashnathashok@gmail.com>
2024-06-09 15:47:42 +0300

    improved the ux by adding a completed option along with the option of viewing it

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 todos = []
+completed_todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, completed or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,14 +11,25 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show" | "display":
-            for item in todos:
+            print(f"Todos left to complete - {len(todos)}")
+            for index, item in enumerate(todos):
                 item = item.title()
-                print(item)
+                index += 1
+                print(f"{index}-{item}")
+            print(f"Todos completed - {len(completed_todos)}")
+            for index, item in enumerate(completed_todos):
+                item = item.title()
+                index += 1
+                print(f"{index}-{item}")
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case "completed":
+            completed = int(input("Enter the number of the completed todo: "))
+            completed -= 1
+            completed_todos.append(todos.pop(completed))
         case "exit":
             break
         case _:
